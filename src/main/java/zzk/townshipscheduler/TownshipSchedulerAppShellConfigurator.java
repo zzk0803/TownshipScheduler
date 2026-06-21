3b56014a839c31477751d77f6d194401920c7430
zzk <zzk0803@outlook.com>
2026-06-21 14:42:28 +0800

    little fix in solverconfig

--- src/main/java/zzk/townshipscheduler/TownshipSchedulerAppShellConfigurator.java
+++ src/main/java/zzk/townshipscheduler/TownshipSchedulerAppShellConfigurator.java
@@ -8,11 +8,16 @@ import com.vaadin.flow.shared.ui.Transport;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 
+import java.io.Serial;
+
 @Push
 @StyleSheet(Lumo.STYLESHEET)
 @StyleSheet(Lumo.UTILITY_STYLESHEET)
 @StyleSheet(value = "styles.css")
 public class TownshipSchedulerAppShellConfigurator
         implements AppShellConfigurator {
+
+    @Serial
+    private static final long serialVersionUID = 1046550348172640564L;
 
 }
