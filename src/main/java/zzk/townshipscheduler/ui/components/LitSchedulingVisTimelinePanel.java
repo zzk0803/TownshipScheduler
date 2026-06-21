883164269824dc07702a38564d2593de5136c4a1
zzk <zzk0803@outlook.com>
2026-06-21 14:21:23 +0800

    frontend dependencies update

--- src/main/java/zzk/townshipscheduler/ui/components/LitSchedulingVisTimelinePanel.java
+++ src/main/java/zzk/townshipscheduler/ui/components/LitSchedulingVisTimelinePanel.java
@@ -15,8 +15,8 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 @Tag("scheduling-vis-timeline-panel")
-@NpmPackage(value = "vis-timeline", version = "8.5.0")
-@NpmPackage(value = "@js-joda/core", version = "5.6.5")
+@NpmPackage(value = "vis-timeline", version = "8.5.1")
+@NpmPackage(value = "@js-joda/core", version = "6.0.1")
 @JsModule("./src/components/scheduling-vis-timeline-panel.ts")
 @JsModule("./src/components/by-factory-timeline-components.ts")
 @JsModule("./src/components/by-order-timeline-components.ts")
