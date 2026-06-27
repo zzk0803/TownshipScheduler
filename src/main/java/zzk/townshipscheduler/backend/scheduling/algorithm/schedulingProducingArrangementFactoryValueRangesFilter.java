be135d8dadb66ad13b8a254db61634eadf3bb128
zzk <zzk0803@outlook.com>
2026-06-27 14:54:11 +0800

    perf:introduce selection filter for arrange what value range of factory amounts more than one

---
+++ src/main/java/zzk/townshipscheduler/backend/scheduling/algorithm/schedulingProducingArrangementFactoryValueRangesFilter.java
@@ -0,0 +1,19 @@
+package zzk.townshipscheduler.backend.scheduling.algorithm;
+
+import ai.timefold.solver.core.impl.heuristic.selector.common.decorator.SelectionFilter;
+import ai.timefold.solver.core.impl.score.director.ScoreDirector;
+import zzk.townshipscheduler.backend.scheduling.model.SchedulingProducingArrangement;
+import zzk.townshipscheduler.backend.scheduling.model.TownshipSchedulingProblem;
+
+public class schedulingProducingArrangementFactoryValueRangesFilter
+        implements SelectionFilter<TownshipSchedulingProblem, SchedulingProducingArrangement> {
+
+    @Override
+    public boolean accept(
+            ScoreDirector<TownshipSchedulingProblem> scoreDirector,
+            SchedulingProducingArrangement selection
+    ) {
+        return selection.getRequiredFactoryInfo().getFactoryInstances().size() > 1;
+    }
+
+}
