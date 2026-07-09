2cc8e0bcda1387f1a8bf958cd08ea1b1f7949690
zzk <zzk0803@outlook.com>
2026-07-09 22:25:10 +0800

    by-unit-timeline-components add factoryId in item

--- src/main/frontend/components/by-unit-timeline-components.ts
+++ src/main/frontend/components/by-unit-timeline-components.ts
@@ -186,7 +186,7 @@ export class ByUnitTimelineComponents
                         dataSetItems.push({
                             id: arrangement?.uuid + '_in_game',
                             group: groupId,
-                            content: `<p class="h-auto w-auto text-center text-2xs"> ${arrangement?.product + "#" + arrangement.id }</p>`,
+                            content: `<p class="h-auto w-auto text-center text-2xs"> ${"(" + arrangement?.factoryReadableIdentifier + ")" +arrangement?.product + "#" + arrangement.id }</p>`,
                             type: 'range',
                             start: arrangement?.producingDateTime,
                             end: arrangement?.completedDateTime,
